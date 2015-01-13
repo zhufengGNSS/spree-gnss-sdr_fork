6ee84eda332c154e5e8dfd0002818e2c392bd185
Hildur <hildur@tildec.inf.ethz.ch>
2015-01-13 16:11:00 +0100

    PVT now works in non-spoof mode

--- src/algorithms/telemetry_decoder/adapters/gps_l1_ca_telemetry_decoder.cc
+++ src/algorithms/telemetry_decoder/adapters/gps_l1_ca_telemetry_decoder.cc
@@ -74,7 +74,10 @@ GpsL1CaTelemetryDecoder::~GpsL1CaTelemetryDecoder()
 void GpsL1CaTelemetryDecoder::set_satellite(Gnss_Satellite satellite)
 {
     satellite_ = Gnss_Satellite(satellite.get_system(), satellite.get_PRN());
+    telemetry_decoder_->set_satellite(satellite_);
+    DLOG(INFO) << "TELEMETRY DECODER: satellite set to " << satellite_;
 }
+
 
 void GpsL1CaTelemetryDecoder::connect(gr::top_block_sptr top_block)
 {
